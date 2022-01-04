1b971f106c684dfbfc772704d30c22034c7c520d
kamarasd <xvidani@hotmail.com>
2022-01-04 18:18:04 +0100

    add test

--- src/test/java/hu/webuni/hr/kamarasd/service/HrTest.java
+++ src/test/java/hu/webuni/hr/kamarasd/service/HrTest.java
@@ -1,25 +1,92 @@
 package hu.webuni.hr.kamarasd.service;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import hu.webuni.hr.kamarasd.web.EmployeeController;
+import hu.webuni.hr.kamarasd.dto.EmployeeDto;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HrTest {
 	
-	@InjectMocks
-	EmployeeService employeeService;
+	private static final String BASE_URI = "api/employees";
 	
-	@Mock
-	EmployeeController employeeController;
-	
+	@Autowired
+	WebTestClient webTestClient;
 	
 	@Test
-	public void testPutEmployee() throws Exception {
+	public void listEmployeesTest() throws Exception {
+		
+		EmployeeDto employee = new EmployeeDto(13, "Teszt Elemér", "Tesztelő", 150000, LocalDateTime.parse("2022-01-04T17:00:00"));
+		
+		
+		List<EmployeeDto> employeeDtoBefore = getEmployees();
+		addEmployee(employee);
+		List<EmployeeDto> employeeDtoAfter = getEmployees();
+		
+		assertThat(!employeeDtoBefore.contains(employee));
+		assertThat(employeeDtoAfter.contains(employee));
+	}
+	
+	@Test
+	public void changeEmployeeTest() throws Exception {
+		EmployeeDto employee = new EmployeeDto(1, "Teszt Elek", "Tesztelő", 150000, LocalDateTime.parse("2022-01-04T17:00:00"));
+				
+				
+		List<EmployeeDto> employeeDtoBefore = getEmployees();
+		EmployeeDto modifiedEmployeeDto = changeEmployee(employee);
+		List<EmployeeDto> employeeDtoAfter = getEmployees();
+		
+		assertThat(modifiedEmployeeDto.equals(employee));
+		assertThat(!employeeDtoBefore.contains(employee));
+		assertThat(employeeDtoAfter.contains(employee));
+	}
+	
+	private EmployeeDto changeEmployee(EmployeeDto employeeDto) {
+		String uri = BASE_URI + "/" + employeeDto.getEmployeeId();
+		
+		return webTestClient
+				.put()
+				.uri(uri)
+				.bodyValue(employeeDto)
+				.exchange()
+				.expectStatus()
+				.isOk()
+				.expectBody(EmployeeDto.class)
+				.returnResult()
+				.getResponseBody();
+	}
+	
+	private List<EmployeeDto> getEmployees() {
+			
+			return webTestClient
+						.get()
+						.uri(BASE_URI)
+						.exchange()
+						.expectStatus()
+						.isOk()
+						.expectBodyList(EmployeeDto.class)
+						.returnResult()
+						.getResponseBody();
+		}
+	
+	private void addEmployee(EmployeeDto employeeDto) {
+		
+		webTestClient
+			.post()
+			.uri(BASE_URI)
+			.bodyValue(employeeDto)
+			.exchange()
+			.expectStatus()
+			.isOk();	
 		
 	}
 
