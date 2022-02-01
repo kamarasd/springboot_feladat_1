1039fd5771c864bad209979a3c2fcf2cdb66aadd
kamarasd <xvidani@hotmail.com>
2022-02-01 18:59:17 +0100

    fixes

--- src/main/java/hu/webuni/hr/kamarasd/service/EmployeeServiceClass.java
+++ src/main/java/hu/webuni/hr/kamarasd/service/EmployeeServiceClass.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
 import hu.webuni.hr.kamarasd.model.Employee;
@@ -68,7 +69,6 @@ public class EmployeeServiceClass {
 	@Transactional
 	public void setPositionForEmployee(Employee employee) {
 		String posName = employee.getPosition().getPosName();
-		//System.out.println(posName);
 		Position position;
 		
 		if(posName != null) {
@@ -83,27 +83,32 @@ public class EmployeeServiceClass {
 	}
 	
 	public List<Employee> findEmployeeByExample(Employee employee) {
+		LocalDateTime defTime = LocalDateTime.of(0001, 01, 01, 00, 00, 00);
+		LocalDateTime workingDate = null;
+		
 		Long id = employee.getEmployeeId() != null ? employee.getEmployeeId() : 0;
 		String name = employee.getName() != null ? employee.getName() : "";
 		String post = employee.getPosition().getPosName() != null ? employee.getPosition().getPosName() : "";
 		int salary = employee.getSalary() != null ? employee.getSalary() : 0;
-		LocalDateTime workingDate = LocalDateTime.of(0001, 01, 01, 00, 00, 00);
+		 
 		if(employee.getWorkingDate() != null) {
 			workingDate = employee.getWorkingDate();
+		} else {
+			workingDate = defTime;
 		}
-		String companyName = employee.getCompany() != null ? employee.getCompany().getCompanyName() : "";
-		
+		String companyName = !employee.getCompany().getCompanyName().isEmpty() ? employee.getCompany().getCompanyName() : "";
+
 		Specification<Employee> spec = Specification.where(null);
 		
 		if(id > 0 && id != null) {
 			spec = spec.and(DinamicEmployeeSearchService.hasId(id));
 		}
 		
-		if(StringUtils.hasText(name) && name != null) {
+		if(StringUtils.hasText(name) && !name.isEmpty()) {
 			spec = spec.and(DinamicEmployeeSearchService.nameStartingWithIgnoreCase(name));
 		}
 		
-		if(StringUtils.hasText(post) && post != null) {
+		if(StringUtils.hasText(post) && !post.isEmpty()) {
 			spec = spec.and(DinamicEmployeeSearchService.hasPost(post));
 		}
 		
@@ -111,15 +116,24 @@ public class EmployeeServiceClass {
 			spec = spec.and(DinamicEmployeeSearchService.hasSalary(salary));
 		}
 		
-		if(workingDate != LocalDateTime.of(0001, 01, 01, 00, 00, 00)) {
+		if(workingDate.isAfter(defTime)) {
 			spec = spec.and(DinamicEmployeeSearchService.startWorkingDate(workingDate));
 		}
 		
-		if(StringUtils.hasText(companyName) || companyName != null) {
+		if(StringUtils.hasText(companyName) || !companyName.isEmpty()) {
 			spec = spec.and(DinamicEmployeeSearchService.hasCompany(companyName));
 		}
 		
 		return employeeRepository.findAll(spec, Sort.by("employeeId"));
+		
+//		Empty test call should be: 
+//	    {
+//	        "name": "",
+//	        "post": "",
+//	        "salary": "",
+//	        "workingDate": "0001-01-01T00:00:00",
+//	        "company": { "companyName" : ""}
+//	    }
 	}
 	 
 	
